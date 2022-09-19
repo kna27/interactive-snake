63696968738f7d110b6e210fcaaea85a46302a16
kna27 <krisharora27@gmail.com>
2022-09-19 15:56:44 -0400

    Add functionality to main menu button

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    public bool moveUp;
+    public bool moveLeft;
+    public bool moveRight;
+    public bool moveDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
