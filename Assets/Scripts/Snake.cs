de2f28604018329c586962cb0f0dd2e685c98f36
kna27 <krisharora27@gmail.com>
2022-10-03 15:55:38 -0400

    Remake snake movement using only left and right

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 public class Snake : MonoBehaviour
 {
     public bool moveUp;
@@ -12,35 +13,50 @@ public class Snake : MonoBehaviour
     public Slider leftSlider;
     public Slider rightSlider;
     public Vector2 _direction = Vector2.right;
+    float timePassed;
+    public float moveDelay = 0.5f;
+    public Vector2[] directions;
+    public int rotationIndex;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector2[] directions = { Vector2.right, Vector2.down, Vector2.left, Vector2.up};
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         leftSlider.value = leftInput;
         rightSlider.value = rightInput;
-        if(Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            _direction = Vector2.up;
-        } else if (Input.GetKeyDown(KeyCode.S))
+            rotationIndex--;
+        } else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            _direction = Vector2.down;
-        } else if (Input.GetKeyDown(KeyCode.A))
-        {
-            _direction = Vector2.left;
-        } else if (Input.GetKeyDown(KeyCode.D))
-        {
-            _direction = Vector2.right;
+            rotationIndex++;
         }
+        if (rotationIndex < 0)
+        {
+            rotationIndex = 3;
+        } else if (rotationIndex > 3)
+        {
+            rotationIndex = 0;
+        }
+
     }
 
     private void FixedUpdate()
     {
-        transform.position = new Vector2(Mathf.Round(transform.position.x) + _direction.x, Mathf.Round(transform.position.y + _direction.y));
-       // transform.position = new Vector2(Mathf.Round(transform.position.x) + )
+    if (timePassed == 0)
+    {
+        transform.position = new Vector2(Mathf.Round(transform.position.x) + directions[rotationIndex].x, Mathf.Round(transform.position.y + directions[rotationIndex].y));
+    }
+        timePassed += Time.deltaTime;
+        if (timePassed >= moveDelay)
+        {
+            timePassed = 0;
+        }
+        // transform.position = new Vector2(Mathf.Round(transform.position.x) + )
     }
 }
